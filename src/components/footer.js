59e93e14a65b313f30efcff60ffd259749a5d192
toptecshare <eestimorol.ice9208g@gmail.com>
2022-02-25 22:40:30 -0500

    target blank for social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,21 @@
-import twitter from "../img/OpenSea.png"
-import discord from "../img/Discord.png"
-import opensea from "../img/Twitter.png"
+import twitter from "../img/OpenSea.png";
+import discord from "../img/Discord.png";
+import opensea from "../img/Twitter.png";
 
 export default function () {
   return (
     <footer>
       <div>
-        <a href="#">
+        <a href="#" target="_blank">
           <img src={twitter} />
         </a>
-        <a href="#">
+        <a href="#" target="_blank">
           <img src={discord} />
         </a>
-        <a href="#">
+        <a href="#" target="_blank">
           <img src={opensea} />
         </a>
       </div>
     </footer>
-  )
+  );
 }
