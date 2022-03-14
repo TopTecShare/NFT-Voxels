04eac4644e4b753ced1df42ea04e0b083742bef5
0xRise <moonrise.techs@gmail.com>
2022-03-14 15:05:38 +0700

    update public sale timer

--- src/pages/publicsalemint.js
+++ src/pages/publicsalemint.js
@@ -1,10 +1,22 @@
+import Countdown from "../components/countdown";
 import CubeMint from "../components/cubemint";
+import useCountdownTimer from "../hooks/useCountdownTimer";
+import { useMintStartTimestamp } from "../hooks/useVoxelsNFT";
 
 import "../styles/publicsalemint.css";
 
 const PresaleWait = () => {
+  const startTimestamp = useMintStartTimestamp();
+  const { timerShowed, duration } = useCountdownTimer(startTimestamp);
   return (
     <div className="publicsalemint">
+      {timerShowed && (
+        <div className="publicsalemint-time">
+          <div className="line pc-style" />
+          <p className="agency pre-content">PUBLIC SALE WILL OPEN IN</p>
+          <Countdown duration={duration} />
+        </div>
+      )}
       <CubeMint isPublic={true} />
     </div>
   );
