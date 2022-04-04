f47afb2b381906563ff0d5fbb8fbc2d265f6df37
toptecshare <eestimorol.ice9208g@gmail.com>
2022-04-03 18:46:59 -0700

    comment out dashboard

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="cube">{/* <Cube /> */}</div>
-      <Dashboard />
+      {/* <Dashboard /> */}
     </div>
   );
 };
