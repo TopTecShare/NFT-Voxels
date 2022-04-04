353150b40bb92360daf829095def4e360759ea26
toptecshare <eestimorol.ice9208g@gmail.com>
2022-04-03 23:21:12 -0700

    background

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,12 @@
 // import Cube from "../components/cube"
-// import Dashboard from "../components/dashboard";
+import Dashboard from "../components/dashboard";
 import "../styles/home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="cube">{/* <Cube /> */}</div>
-      {/* <Dashboard /> */}
+      <Dashboard />
     </div>
   );
 };
